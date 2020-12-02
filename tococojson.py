b14a9da095b989d8b83b7405bac80440770dd0c4
shenhsinyu <73391791+shenhsinyu@users.noreply.github.com>
2020-12-02 19:56:04 +0800

    Update tococojson.py

--- tococojson.py
+++ tococojson.py
@@ -4,22 +4,23 @@ Created on Fri Nov 27 16:10:32 2020
 
 @author: user
 """
-
+import numpy as np
 import json
+
 with open('D:\\predict1.json') as f:
     data = json.load(f)
 with open('D:\\test.json') as f:
     test = json.load(f)
 file_name = test['images']
-for i in range(0,100):
+for i in range(0, 100):
     f = data[i]['image_id']   
-    for j in range(0,100):
+    for j in range(0, 100):
         ff = file_name[j]['file_name']
         if (f == ff[0:-4]):
             data[i]['image_id'] = file_name[j]['id']
-import numpy as np
+
 json_file = []        
-for i in range(0,100):
+for i in range(0, 100):
     out = {}
     if(len(data[i]['score']) == 1):
         out['image_id'] = data[i]['image_id']
